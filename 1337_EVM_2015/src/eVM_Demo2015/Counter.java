b468a2645483ecbe18d855e07c49dc7b681f9b60
rahuldcosta <rahuldc999@gmail.com>
2015-07-15 17:21:51 +0530

    Counter and OnLamp Updated

--- 1337_EVM_2015/src/eVM_Demo2015/Counter.java
+++ 1337_EVM_2015/src/eVM_Demo2015/Counter.java
@@ -2,8 +2,25 @@ package eVM_Demo2015;
 
 public class Counter {
 
+	int tvotes;
+	public int getTvotes() {
+		return tvotes;
+	}
+
 	public Counter() {
+		tvotes=0;
 		// TODO Auto-generated constructor stub
 	}
 
+	public void addvote()
+	{
+		tvotes++;
+	}
+	
+	
+	public void resetcounters()
+	{
+		System.out.println("Reseting the Counters .......");
+		tvotes=0;
+	}
 }
--- 1337_EVM_2015/src/eVM_Demo2015/OnLamp.java
+++ 1337_EVM_2015/src/eVM_Demo2015/OnLamp.java
@@ -2,10 +2,29 @@ package eVM_Demo2015;
 
 public class OnLamp {
 
+	Boolean onstat;
+	public Boolean getOnstat() {
+		return onstat;
+	}
+	
 	public OnLamp() {
 		// TODO Auto-generated constructor stub
-		
-		
+		System.out.println("On Lamp Glowing .....");
+		onstat=true;
 	}
+	
+	public void switchonlamp()
+	{
+
+		if(onstat)
+		{	onstat=false;
+			System.out.println("Beep...............");
+		
+		}
+		else
+			onstat=true;
+	}
+	
+	
 
 }
